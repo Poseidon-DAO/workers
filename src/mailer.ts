4ae1c1c27064957f27d6c718a5a588afb6821399
Patrick Jusic <patrickjusic96@gmail.com>
2022-07-06 19:13:21 +0200

    Drop postmark drop using directly fetch API to send emails through Postmark

--- src/mailer.ts
+++ src/mailer.ts
@@ -1,20 +1,30 @@
-import postmark from "postmark";
-
 const sendEmail = async (from: string, to: string, subject: string, body: string): Promise<void> => {
-  const client = new postmark.ServerClient(POSTMARK_TOKEN);
-  
-  await client.sendEmail({
-    "From": from,
-    "To": to,
-    "Subject": subject,
-    "TextBody": body
-  });
+  await fetch("https://api.postmarkapp.com/email", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "accept": "application/json",
+      "X-Postmark-Server-Token": POSTMARK_TOKEN
+    },
+    body: JSON.stringify({
+      "From": from,
+      "To": to,
+      "Subject": subject,
+      "TextBody": stripHtml(body),
+      "HtmlBody": body,
+      "MessageStream": "outbound"
+    }),
+  })
 };
+
+const stripHtml = (html: string): string => {
+  return html.replace(/<[^>]*>?/gm, '');
+}
 
 const formatArtistApplication = (artist: Artist): string => {
   return `<html>
   <body>
-    <h1>New ${artist.project ? artist.project.charAt(0).toUpperCase() + artist.project.slice(1) + " " : ""}Artist Application</h1></br>
+    <h1>New ${artist.project && artist.project !== "Other" ? artist.project.charAt(0).toUpperCase() + artist.project.slice(1) + " " : ""}Artist Application</h1></br>
     <h3>${artist.name} - ${artist.email}</h3></br>
     <h2>Bio</h2>
     <p>${artist.bio}</p>
